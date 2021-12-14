5f993c1c45acd2229cf727e0d2484032599bd69a
thugdaddy <higginst@avonoldfarms.com>
2021-12-14 11:02:27 -0500

    Update App.js
    Converter App

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableWithoutFeedbackBase,
+} from "react-native";
 
 export default function App() {
+  const [text, onChangeText] = React.useState("");
+  const [number, onChangeNumber] = React.useState(null);
+  const [textbox, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{
+          uri: "https://helios-i.mashable.com/imagery/articles/00apgKgIAO4EnFfjOgCApRe/hero-image.fill.size_1248x702.v1619086604.jpg",
+        }}
+        style={{ width: 300, height: 200 }}
+      />
+      <Text style={styles.jkl}>1.00 USD = 4.65 DOGE</Text>
       <StatusBar style="auto" />
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          placeholder="USD"
+          onChangeText={(textbox) => setText(textbox)}
+          value={textbox}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={textbox * 4.65}
+          placeholder="DOGE"
+          keyboardType="numeric"
+        />
+      </SafeAreaView>
     </View>
   );
 }
@@ -14,8 +46,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffabab",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  jkl: {
+    fontSize: 23,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
